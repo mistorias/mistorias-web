bd86b0a184d7ef5fe2e95e4a381ad643fed4cbf0
Paolo Carrasco <paolo.carrasco@gmail.com>
2026-07-22 00:31:55 +0200

    build: la aplicación se despliega siempre a Github Pages y se publica en Netlify
    - Se configura la aplicación para que las referencias a archivos apunten donde corresponda según el proveedor a donde se despliegue/publique.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 import { defineConfig } from "astro/config";
 
+const deploymentTarget = process.env.DEPLOY_TARGET ?? "github-pages";
+const isGitHubPages = deploymentTarget === "github-pages";
+
 export default defineConfig({
-  srcDir: "src"
+  srcDir: "src",
+  site: isGitHubPages
+    ? "https://mistorias.github.io"
+    : "https://mistorias.pe",
+  base: isGitHubPages ? "/mistorias-web" : "/"
 });
